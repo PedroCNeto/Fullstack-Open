9328020c137b2e88e0217b65de31368d38132815
PedroCNeto <pedrocastelarineto@gmail.com>
2025-02-05 18:28:15 -0300

    P-1-E-2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+const App = () => {
+  const course = 'Desenvolvimento de aplicação Half Stack'
+  const part1 = 'Fundamentos da biblioteca React'
+  const exercises1 = 10
+  const part2 = 'Usando props para passar dados'
+  const exercises2 = 7
+  const part3 = 'Estado de um componente'
+  const exercises3 = 14
 
-function App() {
+  const Header = (props) => {
+    return(
+      <div>
+        <h1>{props.course}</h1>
+      </div>
+    )
+  }
+
+  const Part = (props) => {
+    return(
+      <p>
+        {props.part} {props.exercises}
+      </p>
+    )
+  }
+
+  const Content = (props) => {
+    return(
+      <div>
+        <Part part={props.part1} exercises={props.exercises1}/>
+        <Part part={props.part2} exercises={props.exercises2}/>
+        <Part part={props.part3} exercises={props.exercises3}/>
+      </div>
+    )
+  }
+
+  const Total = (props) => {
+    return(
+      <div>
+        <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      </div>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header course={course} />
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} /> 
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
