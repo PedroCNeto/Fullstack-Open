fadaa0846fc39c2b725f6bb44d1ae800f0cef9a0
PedroCNeto <pedrocastelarineto@gmail.com>
2025-02-26 09:30:30 -0300

    Correcoes simples 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -26,9 +26,7 @@ const App = () => {
   const Content = (props) => {
     return(
       <div>
-        <Part part={props.part1} exercises={props.exercises1}/>
-        <Part part={props.part2} exercises={props.exercises2}/>
-        <Part part={props.part3} exercises={props.exercises3}/>
+        <Part part={props.part} exercises={props.exercises}/>
       </div>
     )
   }
@@ -36,7 +34,7 @@ const App = () => {
   const Total = (props) => {
     return(
       <div>
-        <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+        <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
       </div>
     )
   }
@@ -44,7 +42,9 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} /> 
+      <Content part={part1} exercises={exercises1} /> 
+      <Content part={part2} exercises={exercises2} /> 
+      <Content part={part3} exercises={exercises3} /> 
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
